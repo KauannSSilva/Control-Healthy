27fbdffadc03d77c1e551b36d437d050246dccb0
Kauann Santos <120744603+KauannSSilva@users.noreply.github.com>
2025-06-20 23:57:22 -0300

    Delete php directory

--- php/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start(); // Inicia a sessão para poder destruí-la
-session_unset(); // Remove todas as variáveis de sessão
-session_destroy(); // Destrói a sessão
-
-header("Location: ../html_css/index.html"); // Redireciona para a página inicial
-exit();
-?>
